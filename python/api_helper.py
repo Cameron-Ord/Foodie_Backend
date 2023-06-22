fbaf2a6071b1d521bba5e846b47e0f2e4f6543ce
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-06-22 17:08:52 -0600

    reverted APIHELPER changes

--- python/api_helper.py
+++ python/api_helper.py
@@ -2,7 +2,7 @@ def check_endpoint_info(sent_data, expected_data):
     try:    
         for data in expected_data:
             if(sent_data.get(data) == None):
-                sent_data.get(data) == None
+                return f'the {data} must be sent!'
     except TypeError:
         print('Invalid entry. (how could this happen?)')
     except:
